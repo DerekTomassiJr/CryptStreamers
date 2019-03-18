074bcf2024206739f0c0a358b2f5b22ebd57eda8
Benjamin Tibnam <BenTibnam@users.noreply.github.com>
2019-03-18 09:15:52 -0400

    added new features
    - program now loops
    - added quit functions
    - automatically puts .csf file on input
    - allows user to list all .csf files in current directory
    - allows user to delete all .csf files in current directory

--- main.cpp
+++ main.cpp
@@ -5,22 +5,36 @@ using namespace std;
 //list of methods used throughout the program
 void newFile();
 void loadFile();
+void readDirectory();
+void removeCsf();
 
 int main() {
     int usrInp;
-
+	
     cout << "1. Create New File" << endl;
     cout << "2. Load File" << endl;
+    cout << "3. List .csf file is current directory" << endl;	// TODO add function to windows
+    cout << "4. Remove all .csf files" << endl;
+    cout << "5. Quit" << endl;
+    cout << ">>> " << flush;
     cin >> usrInp;
 
     //determining which method to call
     if (usrInp == 1) {
         newFile();
-    } else {
+        main();
+    }else if(usrInp == 2){
         loadFile();
-    }
-
-    return 0;
+        main();
+    }else if(usrInp == 3){
+    	readDirectory();
+    	main();
+    }else if(usrInp == 4){
+    	removeCsf();
+    	main();
+    }else{
+		return 0;
+	}
 }
 
 void newFile() {
@@ -31,7 +45,7 @@ void newFile() {
     cin >> filename;
     cout << endl;
 
-    outFile.open(filename);
+    outFile.open(filename+".csf");
 
     int favCryptStreamer;
 
@@ -41,18 +55,26 @@ void newFile() {
     cout << "2. Chris Goblin" << endl;
     cout << "3. NebulaIsMe" << endl;
     cout << "4. Summit_Vegetard" << endl;
+    cout << ">>> ";
 
     cin >> favCryptStreamer;
 
     //determinging all cases
-    if (favCryptStreamer == 1) {
-        outFile << "1 cunt" << endl;
-    } else if (favCryptStreamer == 2) {
-        outFile << "2 cunt" << endl;
-    } else if (favCryptStreamer == 3) {
-        outFile << "3 cunt" << endl;
-    } else {
-        outFile << "4 cunt" << endl;
+    switch(favCryptStreamer){
+    case 1:
+    	outFile << "1 cunt" << endl;
+    	break;
+    case 2:
+    	outFile << "2 cunt" << endl;
+    	break;
+    case 3:
+    	outFile << "3 cunt" << endl;
+    	break;
+    case 4:
+    	outFile << "4 cunt" << endl;
+    	break;
+    default:
+    	outFile << "sorry, the user of this file is an idiot who cannot follow simple instructions" << endl;
     }
 
     cout << "File Saved" << endl;
@@ -69,24 +91,75 @@ void loadFile() {
 
     inFile.open(filename);
     if (!inFile.is_open()) {
-        cout << "Invalid Filename." << endl;
-        exit(1);
+    	if(filename == "quit"){
+    		exit(0);
+    	}
+    	
+        cout << "Invalid Filename: \"" << filename << "\". Try again or type \"quit\" to exit program.\n" << endl;
+        loadFile();
     }
 
     int favCryptStreamer;
     inFile >> favCryptStreamer;
 
     cout << "Your Favorite Crypt Streamer is: ";
+    
     //determining case
-    if (favCryptStreamer == 1) {
-        cout << "kevin_crypt" << endl;
-    } else if (favCryptStreamer == 2) {
-        cout << "Chris Goblin" << endl;
-    } else if (favCryptStreamer == 3) {
-        cout << "NebulaIsMe" << endl;
-    } else {
-        cout << "Summit_Vegetard" << endl;
+    switch(favCryptStreamer){
+    case 1:
+    	cout << "kevin_crypt" << endl;
+    	break;
+    case 2:
+    	cout << "Chris Goblin" << endl;
+    	break;
+    case 3:
+    	cout << "NebulaIsMe" << endl;
+    	break;
+    case 4:
+    	cout << "Summit_Vegetard" << endl;
+    	break;
+    default:
+    	cout << "Unknown ID: " << favCryptStreamer << endl;
+    	break;
     }
+    
+    cout << endl;
 
     inFile.close();
+}
+
+void readDirectory(){
+	#ifdef _WIN32
+	system("dir");
+	#elif _WIN64
+	system("dir");
+	#elif __unix
+	system("ls *.csf -a");
+	#elif __APPLE__
+	system("ls *.csf -a");
+	#elif __MACH__
+	system("ls *.csf -a");
+	#elif __linux__
+	system("ls *.csf -a");
+	#elif __FreeBSD__
+	system("ls *.csf -a");
+	#endif
+}
+
+void removeCsf(){
+	#ifdef _WIN32
+	system("del *.csf");
+	#elif _WIN64
+	system("del *.csf");
+	#elif __unix
+	system("rm -v *.csf");
+	#elif __APPLE__
+	system("rm -v *.csf");
+	#elif __MACH__
+	system("rm -v *.csf");
+	#elif __linux__
+	system("rm -v *.csf");
+	#elif __FreeBSD__
+	system("rm -v *.csf");
+	#endif
 }
